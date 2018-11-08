1243973a531b30d5d16699bb1c725cdb39acb66b
Ahmad Saufwan bin Sazali <asfwan@gmail.com>
2018-11-09 01:35:16 +0800

    Table class standardization

--- test2/Models/Table.php
+++ test2/Models/Table.php
@@ -2,14 +2,36 @@
 namespace Test2\Models;
 
 class Table{
-    public static class open(){
-        echo "<table style='border:black 1.5px solid'>";
+    public static function getOpen(){
+        return "<table class='table table-bordered'>";
     }
 
-    public static class close(){
-        echo "</table>";
+    public static function open(){
+        echo static::getOpen();
     }
 
+    public static function getClose(){
+        return "</table>";
+    }
 
-    
+    public static function close(){
+        echo static::getClose();
+    }
+
+    public static function getRow($content){
+        return "<tr>". $content . "</tr>";
+    }
+
+    public static function row($content){
+        echo static::getRow($content);
+    }
+
+    public static function getColumn($content){
+        return "<td>". $content . "</td>";
+    }
+
+    public static function column($content){
+        echo static::getColumn($content);
+    }
+
 }
