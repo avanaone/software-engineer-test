59e417eccbe64a53dbf89eb388e0f84a3f95abb0
aidilrozi <mohd.aidilrozi@gmail.com>
2019-07-31 15:11:37 +0800

    modify function to be as one function

--- test1/public/index.php
+++ test1/public/index.php
@@ -1,15 +1,14 @@
 <?php
 require '../app/bootstrap.php';
-use App\Lib\Bcheck;
-use App\Controllers\DocumentController;
+require './checker.php';
+
 
 $base_url = "http://localhost:7070";
 $inputString="a (b c (d e (f) g) h) i (j k)";
 $bracketIndex = 2;
 $toMatch = '(';
-$bCheck = new Bcheck($inputString,$bracketIndex,$toMatch);
-echo $inputString.'<br>';
-echo 'index of matching bracket is: '.$bCheck->pairIndex();
+$pairedIndex = pairIndex($inputString,$bracketIndex,$toMatch);
+echo 'index of matching bracket is: '.$pairedIndex;
 
 
 
